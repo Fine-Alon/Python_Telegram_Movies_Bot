9f2b7447e053547d7f890970c9310874b645bd3c
Alon Fine <mecheniy5462@gmail.com>
2024-09-08 02:24:10 +0300

    add site request that return response with info of film. Method 'get_movie_by_name' in 'RequestSiteApi' was updated

--- tg_api/handlers/find_film.py
+++ tg_api/handlers/find_film.py
@@ -2,6 +2,7 @@ from loader import bot
 from telebot.types import Message
 from telebot import TeleBot
 from tg_api.states.find_film import FindFilmState
+from website_api.core import RequestSiteApi
 
 
 @bot.message_handler(commands=['find_film'])
@@ -13,8 +14,6 @@ def find_movie(bot: TeleBot, message: Message) -> None:
 
 @bot.message_handler(state=FindFilmState.film)
 def get_film_name(message: Message) -> None:
-    print('message.chat.id: ', message.chat.id)
-    print('message.from_user.id: ', message.from_user.id)
     bot.send_message(message.chat.id, 'Well, I\'ve got it!\nPlease write the genre of the film')
     bot.set_state(message.from_user.id, FindFilmState.genre, message.chat.id)
 
@@ -30,5 +29,12 @@ def get_film_genre(message: Message) -> None:
         #         here site Api fnc
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['genre'] = message.text
+            film = data.get('film')
+            genre = message.text
+
+        # Создаём экземпляр класса RequestSiteApi
+        api_client = RequestSiteApi()
+        # возможно буду передавать словарь парамтров
+        api_client.get_movie_by_name()
     else:
         bot.send_message(message.chat.id, 'Genre must be letters only!')
--- website_api/core.py
+++ website_api/core.py
@@ -4,6 +4,7 @@ from secret import token, f
 print(token)
 API_WEBSITE_KEY = f.decrypt(token.decode())
 
+
 class RequestSiteApi:
     _URL = 'https://api.kinopoisk.dev/v1.4/movie'
     params = {
@@ -12,15 +13,23 @@ class RequestSiteApi:
         'query': 'Рокки'
     }
 
-    def get_movie_by_name(self, params):
+    def get_movie_by_name(self, outcome_params=None):
         """
        Выполняет запрос к API Кинопоиска для поиска фильмов по имени.
 
        :return: JSON-ответ от API с результатами поиска.
        """
+
+        # Объединяем дефолтные параметры с новыми
+        if outcome_params is None:
+            outcome_params = {}
+
+        final_params = self.params.copy()  # Создаем копию дефолтных параметров
+        final_params.update(outcome_params)  # Обновляем копию новыми параметрами
+
         r = requests.get(self._URL + '/search',
                          headers={'X-API-KEY': API_WEBSITE_KEY},
-                         params=params)
+                         params=final_params)
 
         if r.status_code == 200:
             print(r.json())
