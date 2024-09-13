c84eceeffd24b4ff4e6737d5841e20e33ccbea92
Alon Fine <mecheniy5462@gmail.com>
2024-09-13 09:37:17 +0300

    add request by name of film but site API response many movies that have not fully same names

--- tg_api/utils/search_film_creteria_fncs.py
+++ tg_api/utils/search_film_creteria_fncs.py
@@ -7,23 +7,37 @@ api_client = RequestSiteApi()
 
 
 def by_film_name(message: Message):
+    print('by_film_name')
     bot.send_message(message.chat.id, 'Well, I\'ve got it!',
                      reply_markup=ReplyKeyboardRemove())
-    if message.text.isalpha():
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data['film'] = message.text
-        print("Фильм успешно сохранён:", message.text)
 
-        # возможно буду передавать словарь парамтров
-        api_client.get_movie_by_name()
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['film'] = message.text
+    print("Фильм успешно сохранён:", message.text)
+
+    res = api_client.get_movie_by_name(message.text)
+    if res == None:
+        bot.send_message(message.chat.id, 'self titled film wasn\'t found')
     else:
-        bot.send_message(message.chat.id, 'Genre must be letters only!')
-        return
+        print(res)
+        for movie_info in res:
+            # Создаем строку с данными фильма
+            msg = (
+                f"Название: {movie_info.get('Название', 'N/A')}\n"
+                f"Описание: {movie_info.get('Описание', 'N/A')}\n"
+                f"Рейтинг: {movie_info.get('Рейтинг', 'N/A')}\n"
+                f"Год производства: {movie_info.get('Год производства', 'N/A')}\n"
+                f"Жанр: {movie_info.get('Жанр', 'N/A')}\n"
+                f"Возрастной рейтинг: {movie_info.get('Возрастной рейтинг', 'N/A')}\n"
+                f"Постер: {movie_info.get('Постер', 'N/A')}\n"
+            )
+            bot.send_message(message.chat.id, msg)
 
     bot.send_message(message.chat.id, '\nWait a little...')
 
 
 def by_film_rating(message: Message):
+    print("by_film_rating")
     bot.send_message(message.chat.id, 'Well, I\'ve got it!',
                      reply_markup=ReplyKeyboardRemove())
     if message.text.isalpha():
--- website_api/core.py
+++ website_api/core.py
@@ -15,24 +15,43 @@ class RequestSiteApi:
 
     def get_movie_by_name(self, outcome_params=None):
         """
-       Выполняет запрос к API Кинопоиска для поиска фильмов по имени.
+        Выполняет запрос к API Кинопоиска для поиска фильмов по имени и возвращает
+        выбранные параметры: название, описание, рейтинг, год, жанр, возрастной рейтинг, постер.
 
-       :return: JSON-ответ от API с результатами поиска.
-       """
+        :param outcome_params: Дополнительные параметры для поиска.
+        :return: Список фильмов с нужными полями.
+        """
 
         # Объединяем дефолтные параметры с новыми
+        print('outcome_params: ', outcome_params)
         if outcome_params is None:
             outcome_params = {}
-
         final_params = self.params.copy()  # Создаем копию дефолтных параметров
-        final_params.update(outcome_params)  # Обновляем копию новыми параметрами
-
+        print('final_params 1 ', final_params)
+        final_params.update({'query': outcome_params})  # Обновляем копию новыми параметрами
+        print('final_params 2 ', final_params)
         r = requests.get(self._URL + '/search',
                          headers={'X-API-KEY': API_WEBSITE_KEY},
                          params=final_params)
 
         if r.status_code == 200:
-            print(r.json())
-            return r.json()
+            data = r.json().get('docs', [])  # Получаем список фильмов из ответа
+            movie_list = []
+            # Проходимся по каждому фильму и извлекаем нужные поля
+            for movie in data:
+                movie_info = {
+                    'Название': movie.get('name', 'N/A'),
+                    'Описание': movie.get('description', 'N/A'),
+                    'Рейтинг': movie.get('rating', {}).get('kp', 'N/A'),
+                    'Год производства': movie.get('year', 'N/A'),
+                    'Жанр': ', '.join([genre.get('name', 'N/A') for genre in movie.get('genres', [])]),
+                    'Возрастной рейтинг': movie.get('ageRating', 'N/A'),
+                    'Постер': movie.get('poster', {}).get('url', 'N/A')
+                }
+                movie_list.append(movie_info)
+
+            return movie_list
         else:
             print('Response is not OK')
+            return None
+
