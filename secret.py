5153abc5ee72fa3f4663e904887431baa2125e6f
Alon Fine <112291002+Fine-Alon@users.noreply.github.com>
2024-09-17 00:38:56 +0300

    Delete secret.py

--- secret.py
+++
@@ -1,7 +0,0 @@
-from cryptography.fernet import Fernet
-
-API_WEBSITE_KEY = 'P2CDBP4-Z4946D6-KTBTY14-Y28Z30E'
-
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(API_WEBSITE_KEY.encode())
